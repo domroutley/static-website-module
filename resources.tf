144f99e51525cadc61a4859337b8be875d1db576
Dom Routley <dominic.routley@gmail.com>
2020-08-29 18:14:29 +0100

    clean up code, variablise

--- resources.tf
+++ resources.tf
@@ -4,22 +4,17 @@ resource "azurerm_resource_group" "web_rg" {
 }
 
 resource "azurerm_storage_account" "web_sa" {
-  name                     = "websitesdomroutley"
-  resource_group_name      = azurerm_resource_group.web_rg.name
-  location                 = var.location
-  account_kind             = "StorageV2"
-  account_tier             = "Standard"
-  account_replication_type = "GRS"
-  allow_blob_public_access = true
+  name                      = var.storage_account_name
+  resource_group_name       = azurerm_resource_group.web_rg.name
+  location                  = var.location
+  account_kind              = "StorageV2"
+  account_tier              = "Standard"
+  account_replication_type  = "GRS"
+  allow_blob_public_access  = true
   enable_https_traffic_only = false
 
-  # custom_domain {
-  #   name = "dom.routley.info"
-  #   # use_subdomain = true
-  # }
-
   static_website {
-    index_document = "index.html"
+    index_document     = "index.html"
     error_404_document = "404.html"
   }
 }
@@ -32,7 +27,7 @@ resource "azurerm_cdn_profile" "web_cdn_profile" {
 }
 
 resource "azurerm_cdn_endpoint" "web_cdn_endpoint" {
-  name                = "dom"
+  name                = var.endpoint
   profile_name        = azurerm_cdn_profile.web_cdn_profile.name
   location            = var.cdn_location
   resource_group_name = azurerm_resource_group.web_rg.name
--- variables.tf
+++ variables.tf
@@ -1,7 +1,17 @@
 variable "location" {
-  default = "UK South"
+  default     = "UK South"
+  description = "Location for the resource group and storage account."
 }
 
 variable "cdn_location" {
-  default = "West Europe"
+  default     = "West Europe"
+  description = "Location for the CDN profile and endpoint."
+}
+
+variable "endpoint" {
+  description = "CDN endpoint name (XX.azureedge.net)"
+}
+
+variable "storage_account_name" {
+  description = "The name of the storage account, must be globaly unique"
 }
